566ea3620ecf47cd6d7ce950737f98854b19bcfe
pPrachig <amailtoprachi@gmail.com>
2024-09-15 22:33:42 +0530

    add the files

---
+++ src/sept_Fst/ex_14092024/Lab001.java
@@ -0,0 +1,12 @@
+package sept_Fst.ex_14092024;
+
+public class Lab001 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World3");
+        System.out.println("Hello World3");
+        System.out.println("Hello World3");
+        System.out.println("Hello World3");
+
+        }
+}
