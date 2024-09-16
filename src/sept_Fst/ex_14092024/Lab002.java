e96aa2a8dd3032b9f424ff5417d0e8d211168dcb
pPrachig <amailtoprachi@gmail.com>
2024-09-16 20:51:49 +0530

    add the files

---
+++ src/sept_Fst/ex_14092024/Lab002.java
@@ -0,0 +1,14 @@
+package sept_Fst.ex_14092024;
+
+public class Lab002 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World for lab 2");
+        boolean $ = true;
+        boolean _ = false;
+        boolean prachi_hyd = true;
+        boolean _244 = false;
+        System.out.println(_244);
+
+    }
+}
